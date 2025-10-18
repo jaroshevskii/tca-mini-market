eaedd3618cb814bb65fbff58644a2f2a86dc9e86
Sasha Jaroshevskii 🦄 <72662383+jaroshvekii@users.noreply.github.com>
2025-10-18 18:04:04 +0200

    Initial Commit

---
+++ MiniMarket/MiniMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniMarketApp.swift
+//  MiniMarket
+//
+//  Created by Sasha Jaroshevskii on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct MiniMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MiniMarketTests/MiniMarketTests.swift
@@ -0,0 +1,17 @@
+//
+//  MiniMarketTests.swift
+//  MiniMarketTests
+//
+//  Created by Sasha Jaroshevskii on 10/18/25.
+//
+
+import Testing
+@testable import MiniMarket
+
+struct MiniMarketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
